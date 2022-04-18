0be6a8862c2c5cc58748d897fd71e23dc1a9b152
vlamboy <vivienne.lamboy@student.htw-berlin.de>
2022-04-18 14:59:57 +0200

    initial commit

---
+++ src/main/java/de/htwberlin/werbringts/web/HelloWorldController.java
@@ -0,0 +1,16 @@
+package de.htwberlin.werbringts.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping(path = "/")
+    public ModelAndView showHelloWorldPage (){
+        return new ModelAndView("helloworld");
+    }
+
+
+}
---
+++ src/test/java/de/htwberlin/werbringts/WerbringtsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.werbringts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerbringtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
